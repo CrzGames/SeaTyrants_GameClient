8ae8f46ed96e947ddac1b447a009b8fb11f0882d
Corentin <corentindevpro@hotmail.com>
2026-04-11 14:17:04 +0200

    x

--- src/main.cpp
+++ src/main.cpp
@@ -67,7 +67,11 @@ const RC2D_EngineConfig* rc2d_engine_setup(int argc, char* argv[])
     config->callbacks->rc2d_websocket_update = rc2d_websocket_update;
     config->callbacks->rc2d_wake_blocking_threads = rc2d_wake_blocking_threads;
     config->networkClientConfig->channelCount = static_cast<uint32_t>(NetworkChannel::COUNT);
+#if defined(RC2D_PLATFORM_MACOS)
+    config->logicalPresentationMode = RC2D_LOGICAL_PRESENTATION_LETTERBOX;
+#else
     config->logicalPresentationMode = RC2D_LOGICAL_PRESENTATION_OVERSCAN;
+#endif
     config->pixelartMode = false;
     config->appInfo->name = "SeaTyrants";
     config->appInfo->organization = "Crzgames";
